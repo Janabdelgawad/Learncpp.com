6fa30b5a23d3d2174c04e56f413d7ec665a63bbc
JANA <janaakamaal2004@gmail.com>
2024-07-09 01:49:56 +0300

    question 1,2, and 3

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,42 +1,11 @@
-/*
-design:
-goal 
-requirements/problems
-tools/target/backup
-hierarchy
-??
+#include "io.h"
 
-Implementation:
-main
-other functions
-testing
-*/
-
-#include <iostream>
-
-//get(and return) a single integer from the user.
-int readNumber()
-{	
-	std::cout << "Enter a number: ";
-	int input{};
-	std::cin >> input;
-	return input;
-}
-
-//output the answer.take a single parameter and have no return value.
-void writeAnswer(int answer)
-{
-	std::cout << "The sum is: " << answer << "\n";
-}
-
-//A main() function should be used to glue the above functions together.
+//glue the above functions together
 int main()
 {
 	int x{ readNumber() };
 	int y{ readNumber() };
 	int sum = x + y;
 	writeAnswer(sum);
-	
 	return 0;
 }
-
