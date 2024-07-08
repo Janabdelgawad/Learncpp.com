6cd246568bb1b4b2e8a6ba67fdaccac3bf9712c9
JANA <janaakamaal2004@gmail.com>
2024-07-09 01:14:29 +0300

    doesn't compile

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,42 @@
-#include "app.h"
+/*
+design:
+goal 
+requirements/problems
+tools/target/backup
+hierarchy
+??
+
+Implementation:
+main
+other functions
+testing
+*/
+
 #include <iostream>
 
-int add(int x, int y);
+//get(and return) a single integer from the user.
+int readNumber()
+{	
+	std::cout << "Enter a number: ";
+	int input{};
+	std::cin >> input;
+	return input;
+}
 
-int main() {
-	std::cout << add(5, 5);
+//output the answer.take a single parameter and have no return value.
+void writeAnswer(int answer)
+{
+	std::cout << "The sum is: " << answer << "\n";
+}
+
+//A main() function should be used to glue the above functions together.
+int main()
+{
+	int x{ readNumber() };
+	int y{ readNumber() };
+	int sum = x + y;
+	writeAnswer(sum);
+	
 	return 0;
 }
+
