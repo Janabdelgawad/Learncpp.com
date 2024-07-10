3d5b29e5247e9b3f7a82a4e1f0224600dbbd029b
JANA <janaakamaal2004@gmail.com>
2024-07-10 23:06:11 +0300

    another solution

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,46 +1,37 @@
-
 #include <iostream>
 int userInput()
 {
-	std::cout << "Enter an integer ( 0 - 9 ): ";
+	std::cout << "Enter an integer between 0 & 9: ";
 	int input{};
 	std::cin >> input;
-
-	if (input == 2)
-	{
-		return true;
-	}
-	else if (input == 3)
-	{
-		return true;
-	}
-	else if (input == 5)
-	{
-		return true;
-	}
-	else if (input == 7)
-	{
-		return true;
-	}
-	else if (input == 9)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+	return input;
 }
+
+int isPrime(int x)
+{
+    if (x == 2) 
+        return true;
+    else if (x == 3) 
+        return true;
+    else if (x == 5) 
+        return true;
+    else if (x == 7) 
+        return true;
+
+    return false; 
+}
+
 int main()
 {
 	int x{ userInput() };
-	if (userInput)
-	{
-		std::cout << "This digit is prime.\n";
-	}
-	else
-	{
-		std::cout << "this digit is not prime.\n";
-	}
+    int y{ isPrime(x) };
+    if (y)
+    {
+        std::cout << "prime.\n";
+    }
+    else
+    {
+        std::cout << "not prime.";
+    }
 	return 0;
 }
