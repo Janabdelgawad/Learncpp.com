4e43b77d4aa93a2e73802db3ce48da80dd2b51d4
JANA <janaakamaal2004@gmail.com>
2024-07-11 02:05:14 +0300

    revision

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,40 +1,29 @@
 #include <iostream>
-
-int charAsInt(char ch)
+//return passed char as an int
+int charToInt(char x)
 {
-	return ch;
+	return x;
 }
-
-int getInt(int ch)
+//conversion happens when argument is copied to the called function's parameter
+int charThroughIntToInt(int x)
 {
-	return ch;
+	return x;
 }
-
 int main()
 {
-	std::cout << "Enter a single character: ";
-	char ch{};
-	std::cin >> ch;
-	std::cout << "You entered '" << ch << "', which has ASCII code " << static_cast<int>(ch) << ".\n";
+	char ch{ 'a' };
+	std::cout << "NORMAL" << '\t' << "ASCII" << '\n';
+	//didnt want to take input from user, had to check theyre the same value when outputted as expected
+	std::cout << ch << '\t' << static_cast<int>(ch) << '\n';
 
-	//implicit 1
-	int implicit1{ ch };
-	std::cout << "which has ASCII code " << implicit1 << ".\n";
+	//implicit1
+	int charToIntVariable{ ch };
+	std::cout << ch << '\t' << charToIntVariable << '\n';
 
-	//implicit 2
-	std::cout << "which has ASCII code " << charAsInt(ch) << ".\n";
+	//implicit2
+	std::cout << ch << '\t' << charToInt(ch) << '\n';
 
-	//implicit 3
-	std::cout << "which has ASCII code " << getInt(ch) << ".\n";
+	//implicit3 
+	std::cout << ch << '\t' << charThroughIntToInt(ch) << '\n';
 	return 0;
-
 }
-
-/*
-explicit type conversion (favored) using static_cast<type>(identifier)
-implicit type conversion (unfavored) using:
-1)new variable type int & store a char in it
-2) a function to return the char variable as an int
-3) a function that performs the conversion at the point where 
-   the argument passes its value to the function parameter 
-*/
