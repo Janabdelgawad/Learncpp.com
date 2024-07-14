9bec6b13d86188ac4e222e463f93e35de86c9b32
JANA <janaakamaal2004@gmail.com>
2024-07-14 17:38:24 +0300

    guide answer

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,57 +1,59 @@
-
-// The program computes the answer on the two numbers the user entered
-// and prints the results.
-// If the user enters an invalid symbol,
-// the program should print nothing.
-
-//Example of program :
 /*
-Enter a double value : 6.2
-Enter a double value : 5
-Enter + , -, *, or / : *
-6.2 * 5 is 31
+To start, the user should be asked for the height of the tower in meters. 
+
+gravity = (9.8 m/s2),
+velocity = 0
+Have the program output the height of the ball above the ground after 0, 1, 2, 3, 4, and 5 seconds. 
+The ball should not go underneath the ground (height 0).
+
+Use a function to calculate the height of the ball after x seconds.
+The function can calculate how far the ball has fallen after x seconds using the following formula:
+distance fallen = gravity_constant * x_seconds2 / 2
 */
 #include <iostream>
-double userInput()
+
+double towerHeight()
 {
-	std::cout << "Enter a double value: ";
+	std::cout << "Enter the height of the tower in meters: ";
 	double input{};
 	std::cin >> input;
 	return input;
 }
 
-char userOperator()
+double calculateBallHeight(double height, int seconds)
 {
-	std::cout << "Enter + , -, *, or / : ";
-	char input{};
-	std::cin >> input;
-	return input;
+	double gravity{ 9.8 };
+
+	double distance{ gravity * (seconds * seconds) / 2.0 };
+	double ballHeight{height - distance};
+
+	//if ball underground, place it on the ground
+	if (ballHeight < 0.0)
+		return 0.0;
+
+	return ballHeight;
 }
 
-double result(double x, double y, char z)
+void printBallHeight(double height, int seconds)
 {
-	if (z == '+')
-		return x + y;
+	if(height > 0.0)
+		std::cout << "At " << seconds << " seconds, the ball is at height : " << height << " meters\n";
+	else
+		std::cout << "At " << seconds << " seconds, the ball is on the ground.\n";
+}
 
-	else if (z == '-')
-		return x - y;
-
-	else if (z == '*')
-		return x * y;
-
-	else if (z == '/')
-		return x / y;
-
-	return 0;
+void calculateAndPrint(double height, int seconds) {
+	printBallHeight(calculateBallHeight(height, seconds), seconds);
 }
 
 int main()
 {
-	double x{ userInput() };
-	double y{ userInput() };
-	char z{ userOperator() };
-	double print{ result(x, y, z) };
-	
-	std::cout << x << ' ' << z << ' ' << y << " is " << print;
+	double height{ towerHeight() } ;
+	calculateAndPrint(height, 0);
+	calculateAndPrint(height, 1);
+	calculateAndPrint(height, 2);
+	calculateAndPrint(height, 3);
+	calculateAndPrint(height, 4);
+	calculateAndPrint(height, 5);
 	return 0;
 }
