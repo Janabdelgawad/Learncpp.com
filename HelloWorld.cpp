a9393a0001baba1b19999e573bc72502f88f9cc3
JANA <janaakamaal2004@gmail.com>
2024-07-20 03:38:14 +0300

    fakdt el amal seeka b3d el constexpr el beeed bs ethafezt b3d ma 3reft a3ml el exercise da lesssgooo

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,46 +1,37 @@
+/*
+function vs member functions
+length(name) vs   name.length()
+member function means that it is part of a library(std::string)
+
+when using std::string when receiving input 
+use std::getline(std::cin >> std::ws, "your string here");
+ws-> ignores white spaces 
+getline -> read a full line of input (not just the first thing like what std::cin does alone)
+constexpr cant use std::string in versions < ++20/23
+don't pass strings by value use std::stringview
+
+*/
+
 #include <iostream>
-
-double TowerHeight()
-{
-	std::cout << "Enter the height of the tower in meters: ";
-	double towerHeight{};
-	std::cin >> towerHeight;
-	return towerHeight;
-}
-
-double CalculateBallHeight(double height, int seconds)
-{
-	double gravity{ 9.8 };
-	double distance{ gravity * (seconds * seconds) / 2.0 };
-	double ballHeight{ height - distance };
-	if (ballHeight < 0.0)
-		return 0.0;
-	return ballHeight;
-}
-
-void PrintBallHeight(double height, int seconds)
-{
-	if (height > 0.0)
-		std::cout << "At " << seconds << " seconds, the ball is at height : " << height << " meters\n";
-	else
-		std::cout << "At " << seconds << " seconds, the ball is on the ground.\n";
-}
-
-void calculateAndPrint(double height, int seconds)
-{
-	double ballHeight{ CalculateBallHeight(height, seconds) };
-	PrintBallHeight(ballHeight, seconds);
-}
+#include <string>
 
 int main()
 {
-	double towerHeight{ TowerHeight() };
-	calculateAndPrint(towerHeight, 0);
-	calculateAndPrint(towerHeight, 1);
-	calculateAndPrint(towerHeight, 2);
-	calculateAndPrint(towerHeight, 3);
-	calculateAndPrint(towerHeight, 4);
-	calculateAndPrint(towerHeight, 5);
+	//get input(user's name)
+	std::cout << "Enter your full name: ";
+	std::string name{};
+	std::getline(std::cin >> std::ws, name); //read a full line of text into name
+
+	//count user's name length
+	int castedNameLength{static_cast<int>(name.length())}; //name.length() is unsigned, convert it to a signed value
+
+	//get input(user's age)
+	std::cout << "Enter your age: ";
+	int age{}; // is signed
+	std::cin >> age;
+
+	//output(user's name length & age)
+	std::cout << "Your age + length of name is: " << castedNameLength + age << '\n';
 
 	return 0;
 }
