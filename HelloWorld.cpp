ce5bf272b5385f44328a3c9f31748e6a46a3530f
JANA <janaakamaal2004@gmail.com>
2024-08-04 01:19:15 +0300

    local variables scope where v is used/seen duration rules govern v creat&destruct lifetime time of v when creat till destruct

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,30 +1,42 @@
+/*
+Variable's
+----------
+Scope: where the variable is seen/used in code
+Duration: the rules that govern when a variable is created&destroyed
+Lifetime: the time between creation&destruction
+
+
+The program output should match the following:
+Enter an integer: 4
+Enter a larger integer: 2
+Swapping the values
+The smaller value is 2
+The larger value is 4
+*/
 #include <iostream>
-
-int getInt()
-{
-	std::cout << "Enter an integer: ";
-	int input{};
-	std::cin >> input;
-	return input;
-}
-
-constexpr bool isEven(int num)
-{
-	return ((num % 2) == 0) ? true : false;
-}
-
-void printNum(bool x, int num)
-{
-	if (x)
-		std::cout << num << " is even";
-	else
-		std::cout << num << " is odd";
-}
-
 int main()
 {
-	const int num{ getInt() };
-	const bool evenOrOdd{ isEven(num) };
-	printNum(evenOrOdd, num);
+	//larger and smaller are created
+	std::cout << "Enter an integer: ";
+	int smaller{};
+	std::cin >> smaller;
+	std::cout << "Enter a larger integer: ";
+	int larger{};
+	std::cin >> larger;
+
+	//if wrong order was entered
+	if (larger < smaller)
+	{
+		std::cout << "Swapping the values\n";
+
+		//temp created
+		int temp{ smaller };
+		smaller = larger;
+		larger = temp;
+	}	//temp destroyed
+
+	std::cout << "The smaller value is: " << smaller << '\n';
+	std::cout << "The larger value is: " << larger << '\n';
+
 	return 0;
-}
+}	//larger and smaller are destroyed
