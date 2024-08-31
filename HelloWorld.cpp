3ceef6753869c9867f02ffbe5aba44b53338757d
JANA <janaakamaal2004@gmail.com>
2024-08-31 04:27:34 +0300

    templates

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,87 +1,44 @@
 #include <iostream>
-#include <limits>
-#include <cstdlib>
-
-void ignoreLine()
+/*
+template<typename T>
+T add(T x, T y)
 {
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-}
-
-int randomNumberGenerator()
-{
-    return 1 + rand() % 100;
-}
-
-bool playAgain()
-{
-    std::cout << "\nWould you like to play again (y/n)?\n";
-    char input{};
-    std::cin >> input;
-    ignoreLine(); // Ignore any extra characters after the input
-    while (input != 'y' && input != 'n')
-    {
-        std::cout << "Invalid input. Would you like to play again (y/n)?\n";
-        std::cin >> input;
-        ignoreLine(); // Ignore any extra characters after the input
-    }
-    return input == 'y';
-}
-
-void Answer(int user, int answer)
-{
-    if (user < answer) std::cout << "Your guess is too low.\n";
-    else if (user > answer) std::cout << "Your guess is too high.\n";
-    else std::cout << "Correct! You win!\n";
-}
-
-int userTrial(int answer)
-{
-    int input{};
-    int i{ 1 };
-
-    std::cout << "Let's play a game. I'm thinking of a number between 1 and 100. You have 7 tries to guess what it is.\n";
-    for (; i <= 7; i++)
-    {
-        std::cout << "\nGuess #" << i << ": ";
-        std::cin >> input;
-
-        if (!std::cin)
-        {
-            std::cin.clear();
-            ignoreLine();
-            std::cout << "Invalid input. Please enter a number between 1 and 100.\n";
-            i--; // Decrement the counter to give the user another chance
-            continue;
-        }
-
-        if (input < 1 || input > 100)
-        {
-            std::cout << "Out of bounds. Please enter a number between 1 and 100.\n";
-            ignoreLine();
-            i--; // Decrement the counter to give the user another chance
-            continue;
-        }
-
-        if (input == answer)
-        {
-            Answer(input, answer);
-            return input;
-        }
-        Answer(input, answer);
-    }
-    std::cout << "\nSorry, you lose. The correct number was " << answer << ".\n";
-    ignoreLine();
-    return input;
+	return x + y;
 }
 
 int main()
 {
-    do
-    {
-        int randomNumber{ randomNumberGenerator() };
-        int userNumber{ userTrial(randomNumber) };
-    } while (playAgain());
+	std::cout << add(2, 3) << '\n';
+	std::cout << add(1.2, 3.4) << '\n';
+	return 0;
+}
+--------------------------
+template <typename T>
+T mult(T x, int y)
+{
+	return x * y;
+}
 
-    std::cout << "Thank you for playing!\n";
-    return 0;
+int main()
+{
+	std::cout << mult(2, 3) << '\n';
+	std::cout << mult(1.2, 3) << '\n';
+
+	return 0;
+}
+*/
+
+
+template<typename T, typename U>
+auto sub(T x, U y)
+{
+	return x - y;
+}
+int main()
+{
+	std::cout << sub(3, 2) << '\n';
+	std::cout << sub(3.5, 2) << '\n';
+	std::cout << sub(4, 1.5) << '\n';
+
+	return 0;
 }
