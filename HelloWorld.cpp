722e70c0cd861764c1ebba196f71c6641f87a8e4
JANA <janaakamaal2004@gmail.com>
2024-10-01 18:30:41 +0300

    quiz 1

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,34 +1,41 @@
 #include <iostream>
-
-template<typename T, typename U>
-	struct Pair 
-	{
-		T num1{};
-		U num2{};
-	};
-
-	struct Point
-	{
-		int num1{};
-		int num2{};
-	};
-
-template<typename T>
-void max(T num)
+struct Monster
 {
-	std::cout << ((num.num1 < num.num2) ? num.num2 : num.num1);
+	enum Type
+	{
+		ogre, dragon, orc, giantSpider, slime,
+	};
+	Type type{};
+	std::string name{};
+	int health;
+};
+
+std::string readMonster(const Monster::Type m)
+{
+	switch (m)
+	{
+	case Monster::ogre:        return "Ogre";
+	case Monster::dragon:      return "Dragon";
+	case Monster::orc:         return "Orc";
+	case Monster::giantSpider: return "Giant Spider";
+	case Monster::slime:       return "Slime";
+	default:                   return "???";
+	}
+}
+
+void printMonster(const Monster m)
+{
+
+	std::cout << "This " << readMonster(m.type) << " is named " << m.name << " and has " << m.health << " health.\n";
 }
 
 int main()
 {
-	Pair<double, int> x1{ 1.3,2 };
-	max(x1);
+	Monster m1{ Monster::ogre, "Torg", 145};
+	Monster m2{ Monster::slime, "Blurp", 23};
 
-	std::cout << '\n';
+	printMonster(m1);
+	printMonster(m2);
 
-	Point x2{ 3,4 };
-	max(x2);
-
-	std::cout << '\n';
 	return 0;
 }
