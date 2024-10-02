52067a03512bf5ea9bbe9f764a233a7de31bc0eb
JANA <janaakamaal2004@gmail.com>
2024-10-02 03:38:46 +0300

    qqq

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,92 +1,32 @@
-// instantiate a snake instead of a cat.
-
-//OOP PROGRAMMING 
 #include <iostream>
-#include <string_view>
-
-struct Cat
+struct IntPair
 {
-    std::string_view name{ "cat" };
-    int numLegs{ 4 };
+	int num1{};
+	int num2{};
+	void print()
+	{
+		std::cout << "Pair(" << num1 << ", " << num2 << ")\n";
+	}
+	bool isEqual(IntPair khara)
+	{
+		return (num1 == khara.num1) && (num2 == khara.num2);
+	}
 };
 
-struct Dog
-{
-    std::string_view name{ "dog" };
-    int numLegs{ 4 };
-};
-
-struct Chicken
-{
-    std::string_view name{ "chicken" };
-    int numLegs{ 2 };
-};
-
-struct Snake
-{
-    std::string_view name{ "snake" };
-    int numLegs{ 0 };
-};
-
+// indicating whether one IntPair is equal to another.
 int main()
 {
-    constexpr Snake animal;
-    std::cout << "a " << animal.name << " has " << animal.numLegs << " legs\n";
+	IntPair p1{ 1, 2 };
+	IntPair p2{ 3, 4 };
 
-    return 0;
+	std::cout << "p1: ";
+	p1.print();
+
+	std::cout << "p2: ";
+	p2.print();
+
+	std::cout << "p1 and p1 " << (p1.isEqual(p1) ? "are equal\n" : "are not equal\n");
+	std::cout << "p1 and p2 " << (p1.isEqual(p2) ? "are equal\n" : "are not equal\n");
+
+	return 0;
 }
-
-
-
-
-
-
-
-/////////PROCEDURAL PROGRAMMING
-
-/*
-#include <iostream>
-#include <string_view>
-
-enum AnimalType
-{
-    cat,
-    dog,
-    chicken,
-    snake,
-};
-
-constexpr std::string_view animalName(AnimalType type)
-{
-    switch (type)
-    {
-    case cat: return "cat";
-    case dog: return "dog";
-    case chicken: return "chicken";
-    case snake: return "snake";
-    default:  return "";
-    }
-}
-
-constexpr int numLegs(AnimalType type)
-{
-    switch (type)
-    {
-    case cat: return 4;
-    case dog: return 4;
-    case chicken: return 2;
-    case snake: return 0;
-    default:  return 0;
-    }
-}
-
-
-int main()
-{
-    constexpr AnimalType animal{ snake };
-    std::cout << "A " << animalName(animal) << " has " << numLegs(animal) << " legs\n";
-
-    return 0;
-}
-*/
-
