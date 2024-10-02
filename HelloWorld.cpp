803573ae6eab3d6ff48f12cf2482c8c735bcc946
JANA <janaakamaal2004@gmail.com>
2024-10-02 03:27:46 +0300

    qq

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,28 +1,92 @@
+// instantiate a snake instead of a cat.
+
+//OOP PROGRAMMING 
 #include <iostream>
-template <typename T>
-struct Triad
+#include <string_view>
+
+struct Cat
 {
-	T first{};
-	T second{};
-	T third{};
+    std::string_view name{ "cat" };
+    int numLegs{ 4 };
 };
 
-template <typename T>
-Triad(T, T, T) -> Triad<T>;
-
-template <typename Print>
-void print(const Triad<Print>& p)
+struct Dog
 {
-	std::cout << '[' << p.first << ", " << p.second << ", " << p.third << ']';
-}
+    std::string_view name{ "dog" };
+    int numLegs{ 4 };
+};
+
+struct Chicken
+{
+    std::string_view name{ "chicken" };
+    int numLegs{ 2 };
+};
+
+struct Snake
+{
+    std::string_view name{ "snake" };
+    int numLegs{ 0 };
+};
 
 int main()
 {
-	Triad t1{ 1, 2, 3 }; // note: uses CTAD to deduce template arguments
-	print(t1);
+    constexpr Snake animal;
+    std::cout << "a " << animal.name << " has " << animal.numLegs << " legs\n";
 
-	Triad t2{ 1.2, 3.4, 5.6 }; // note: uses CTAD to deduce template arguments
-	print(t2);
-
-	return 0;
+    return 0;
 }
+
+
+
+
+
+
+
+/////////PROCEDURAL PROGRAMMING
+
+/*
+#include <iostream>
+#include <string_view>
+
+enum AnimalType
+{
+    cat,
+    dog,
+    chicken,
+    snake,
+};
+
+constexpr std::string_view animalName(AnimalType type)
+{
+    switch (type)
+    {
+    case cat: return "cat";
+    case dog: return "dog";
+    case chicken: return "chicken";
+    case snake: return "snake";
+    default:  return "";
+    }
+}
+
+constexpr int numLegs(AnimalType type)
+{
+    switch (type)
+    {
+    case cat: return 4;
+    case dog: return 4;
+    case chicken: return 2;
+    case snake: return 0;
+    default:  return 0;
+    }
+}
+
+
+int main()
+{
+    constexpr AnimalType animal{ snake };
+    std::cout << "A " << animalName(animal) << " has " << numLegs(animal) << " legs\n";
+
+    return 0;
+}
+*/
+
