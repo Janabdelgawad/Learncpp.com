e07932f857f9e5d369e04234f36322d6c2db605b
JANA <janaakamaal2004@gmail.com>
2024-10-10 04:54:59 +0300

    ccccc

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,42 +1,32 @@
 #include <iostream>
-class Point3d
+class Ball
 {
-    int m_x{};
-    int m_y{};
-    int m_z{};
-
-public:
-    void setValues(int x, int y, int z)
-    {
-        m_x = x;
-        m_y = y;
-        m_z = z;
-    }
+private: 
+    std::string m_color{ "black" };
+    double m_radius{ 10.0 };
     void print() const
     {
-        std::cout << '<' << m_x << ", " << m_y << ", " << m_z << ">\n";
+        std::cout << "Ball(" << m_color << ", " << m_radius << ")\n";
     }
-    bool isEqual(const Point3d& point) const
+
+public:
+    Ball(double radius)
+        :Ball{ "black", radius }
+    {}
+
+    Ball(std::string_view color = "black", double radius = 10.0)
+        : m_color{ color }, m_radius{ radius }
     {
-        return (m_x == point.m_x) && (m_y == point.m_y) && (m_z == point.m_z);
-              
+        print();
     }
 };
 
 int main()
 {
-    Point3d point1{};
-    point1.setValues(1, 2, 3);
-
-    Point3d point2{};
-    point2.setValues(1, 2, 3);
-
-    std::cout << "point 1 and point 2 are" << (point1.isEqual(point2) ? "" : " not") << " equal\n";
-
-    Point3d point3{};
-    point3.setValues(3, 4, 5);
-
-    std::cout << "point 1 and point 3 are" << (point1.isEqual(point3) ? "" : " not") << " equal\n";
+    Ball def{};
+    Ball blue{ "blue" };
+    Ball twenty{ 20.0 };
+    Ball blueTwenty{ "blue", 20.0 };
 
     return 0;
 }
