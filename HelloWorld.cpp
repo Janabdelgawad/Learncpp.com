ea22ea1f2b0066bb45fb95b8e4a5380ead37ea0a
JANA <janaakamaal2004@gmail.com>
2024-10-14 02:47:58 +0300

    Question 1

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,32 +1,35 @@
+#include <cmath>
 #include <iostream>
-class Ball
+class Point2d
 {
-private: 
-    std::string m_color{ "black" };
-    double m_radius{ 10.0 };
+private:
+    double m_x{ 0.0 };
+    double m_y{ 0.0 };
+public:
+    Point2d() = default;
+    Point2d(double x, double y)
+    : m_x{x} , m_y{y}{}
+
     void print() const
     {
-        std::cout << "Ball(" << m_color << ", " << m_radius << ")\n";
+        std::cout << "Point2d("  << m_x << ", " << m_y << ")\n";
     }
 
-public:
-    Ball(double radius)
-        :Ball{ "black", radius }
-    {}
-
-    Ball(std::string_view color = "black", double radius = 10.0)
-        : m_color{ color }, m_radius{ radius }
+    double distanceTo(Point2d& p) const
     {
-        print();
+        return std::sqrt((p.m_x - m_x) * (p.m_x - m_x) + (p.m_y - m_y) * (p.m_y - m_y));
     }
 };
 
 int main()
 {
-    Ball def{};
-    Ball blue{ "blue" };
-    Ball twenty{ 20.0 };
-    Ball blueTwenty{ "blue", 20.0 };
+    Point2d first{};
+    Point2d second{ 3.0, 4.0 };
+
+    first.print();
+    second.print();
+
+    std::cout << "Distance between two points: " << first.distanceTo(second) << '\n';
 
     return 0;
 }
