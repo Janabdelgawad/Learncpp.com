50245567d8dd316e8063ac7c0136e07fe81ca133
JANA <janaakamaal2004@gmail.com>
2024-10-16 22:24:38 +0300

    question 1

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,19 +1,10 @@
-#include <vector>
 #include <iostream>
+#include <vector>
+
 int main()
 {
-	//Q1
-	std::vector<int> v1{1, 4, 9, 16, 25};
-	//Q2 difference between v{2} and v(2), first element's value intialized with 2 and a vector with 2 elements zero initialized
-	//Q3
-	std::vector<double> v2(365);
-
-	//Q4
-	std::cout << "Enter 3 integers: ";
-	std::vector<int> input(3);
-	std::cin >> input[0] >> input[1] >> input[2];
-
-	std::cout << "The sum is: " << input[0] + input[1] + input[2] << '\n';
-	std::cout << "The product is: " << input[0] * input[1] * input[2] << '\n';
+	std::vector v{ 'h', 'e', 'l', 'l', 'o' };
+	std::cout << "The array has " << std::size(v) << " elements.\n";
+	std::cout << v[1] <<v.at(1) << '\n';
 	return 0;
 }
