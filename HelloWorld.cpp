324dac82fee939ac190c46789a338540009c773a
JANA <janaakamaal2004@gmail.com>
2024-10-17 07:54:02 +0300

    bamot

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,45 +1,43 @@
-/*
-Numbers divisible by only 3 should print “fizz”.
-Numbers divisible by only 5 should print “buzz”.
-Numbers divisible by only 7 should print “pop”.
-Numbers divisible by only 11 should print “bang”.
-Numbers divisible by only 13 should print “jazz”.
-Numbers divisible by only 17 should print “pow”.
-Numbers divisible by only 19 should print “boom”.
-Numbers divisible by more than one of the above should print each of the words associated with its divisors.
-Numbers not divisible by any of the above should just print the number.
-*/
-#include <iostream>
-#include <vector>
+// h/t to reader Waldo for suggesting this quiz
 #include <cassert>
+#include <iostream>
+#include <string_view>
+#include <vector>
 
-void fizzBuzz(int count)
+void fizzbuzz(int count)
 {
-	static const std::vector divisors{ 3, 5, 7, 9, 11, 13, 17, 19 };
-	static const std::vector<std::string_view> words{ "fizz", "buzz", "pop", "bang", "jazz", "pow", "boom" };
+	// We'll make these static so we only have to do initialization once
+	static const std::vector divisors                { 3, 5, 7, 11, 13, 17, 19 };
+	static const std::vector<std::string_view> words { "fizz", "buzz", "pop", "bang", "jazz", "pow", "boom" };
 
 	assert(std::size(divisors) == std::size(words) && "fizzbuzz: array sizes don't match");
 
+	// Loop through each number between 1 and count (inclusive)
 	for (int i{ 1 }; i <= count; ++i)
 	{
 		bool printed{ false };
 
+		// Check the current number against each possible divisor
 		for (std::size_t j{ 0 }; j < divisors.size(); ++j)
 		{
-			if (i % divisors[j])
+			if (i % divisors[j] == 0)
 			{
 				std::cout << words[j];
 				printed = true;
 			}
 		}
-		if (!printed) std::cout << i;
+
+		// If there were no divisors
+		if (!printed)
+			std::cout << i;
 
 		std::cout << '\n';
 	}
 }
+
 int main()
 {
+	fizzbuzz(150);
 
-	fizzBuzz(150);
 	return 0;
 }
