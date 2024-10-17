e83f18d7c457cd972ce9ca15ca23451f50dfae47
JANA <janaakamaal2004@gmail.com>
2024-10-17 05:02:46 +0300

    Question 2

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <vector>
-
+ template <typename T>
+ void printArray(const std::vector<T>& arr)
+ {
+     for (std::size_t index{ 0 }; index < arr.size(); ++index)
+         std::cout << arr[index] << ' ';
+ }
 int main()
 {
     std::vector arr{ 4, 6, 7, 3, 8, 2, 1, 9 };
 
-    std::size_t length{ arr.size() };
-    for (std::size_t index{ 0 }; index < length; ++index)
-        std::cout << arr[index] << ' ';
+    printArray(arr); // use function template to print array
 
     return 0;
 }
