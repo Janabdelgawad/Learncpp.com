54842612e650699e20f2529bf9685e7128e908ef
JANA <janaakamaal2004@gmail.com>
2024-10-17 06:02:45 +0300

    Question 5

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,52 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <limits>
- int getValidNumber()
- {
-     int num{};
-     do
-     {
-         std::cout << "Enter a number between 1 and 9: ";
-         std::cin >> num;
-
-         if (!std::cin)
-             std::cin.clear();
-
-         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-     } while (num < 1 || num > 9);
-
-     return num;
- }
-
- template <typename T>
- void printArray(const std::vector<T>& arr)
- {
-     for (std::size_t index{ 0 }; index < arr.size(); ++index)
-         std::cout << arr[index] << ' ';
-     if (arr.size() > 0) std::cout << '\n';
- }
 
 template <typename T>
-int valueChecker(T value, const std::vector<T>& arr)
+T findMax(const std::vector<T>& arr)
 {
     std::size_t length{ arr.size() };
-    for (std::size_t index{ 0 }; index < length; ++index)
-    {
-        if (value == arr[index]) return index;
-        
-    }
-    return -1;
+
+    if (length == 0) return T{};
+
+    T max{ arr[0]};
+    for (std::size_t index{ 1 }; index < length; ++index)
+        if (arr[index] > max) max = arr[index];
+
+    return max;
+
 }
 
 int main()
 {
-    std::vector arr{ 4, 6, 7, 3, 8, 2, 1, 9 };
-    int num{ getValidNumber()};
-    printArray(arr);
-    int index{ valueChecker(num, arr) };
-    if(index == -1) std::cout << "The number " << num << " was not found \n";
-    else     std::cout << "The number " << num << " has index " << index << '\n';
+    std::vector data1{ 84, 92, 76, 81, 56 };
+    std::cout << findMax(data1) << '\n';
+
+    std::vector data2{ -13.0, -26.7, -105.5, -14.8 };
+    std::cout << findMax(data2) << '\n';
+
+    std::vector<int> data3{ };
+    std::cout << findMax(data3) << '\n';
 
     return 0;
 }
