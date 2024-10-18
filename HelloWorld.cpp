5544a54ae1ac5c9b2a9cb855e0bd06c541bba07f
JANA <janaakamaal2004@gmail.com>
2024-10-18 06:15:41 +0300

    Question TWOO LETSGOOOO

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <string_view>
-//Write definitions for the following. Use CTAD where possible
-//
+#include <cassert>
+namespace Item
+{
+	enum ItemTypes
+	{
+		healthPotions, torches, arrows, maxCount
+
+	};
+}
+int totalCount(const std::vector<int>& inventory)
+{
+	int sum{ 0 };
+	for (auto item : inventory)
+		sum += item;
+	return sum;
+}
 int main()
 {
-	std::vector<int> even6{ 2, 4, 6, 8, 10, 12 };
-	const std::vector<double> doubles{ 1.2, 3.4, 5.6, 7.8 };
-	using namespace std::literals::string_view_literals;
-	const std::vector names{"Alex"sv, "Brad"sv, "Charles"sv, "Dave"sv};
-	std::vector value{ 12 };
-	std::vector<int> values(12);
+	std::vector inventory{ 1, 5, 10 };
+	assert(std::size(inventory) == Item::maxCount);
+
+	std::cout << "You have " << totalCount(inventory) << " total items.\n";
 	return 0;
 }
