07fff33f7736a3faa1c8187a35f76ffb121c0599
JANA <janaakamaal2004@gmail.com>
2024-10-18 22:11:28 +0300

    Question 4, gebt el logic sah bs el beed lazm kan yfty w yheshi hagat mn 3ndo hasesni eni m3mltsh haga el zebala

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,71 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <type_traits> 
-#include <cassert>
-namespace Item
-{
-	enum ItemTypes: int
-	{
-		healthPotions, torches, arrows, maxCount
-
-	};
-}
-
-std::string_view getItemNamePlural(Item::ItemTypes type)
-{
-	switch (type)
-	{
-	case Item::healthPotions: return "health potions";
-	case Item::torches: return "torches";
-	case Item::arrows: return "arrows";
-	default: return "???";
-	}
-}
-
-std::string_view getItemNameSingular(Item::ItemTypes type)
-{
-	switch (type)
-	{
-	case Item::healthPotions: return "health potion";
-	case Item::torches: return "torch";
-	case Item::arrows: return "arrow";
-	default: return "???";
-	}
-}
+#include <utility>
 template <typename T>
-constexpr std::size_t toUZ(T value)
+std::pair<std::size_t, std::size_t> minMax(const std::vector<T>& arr)
 {
-	// make sure T is an integral type
-	static_assert(std::is_integral<T>() || std::is_enum<T>());
-
-	return static_cast<std::size_t>(value);
-}
-
-void print(const std::vector<int>& inventory, Item::ItemTypes type)
-{
-		bool plural{ inventory[toUZ(type)] != 1 };
-		std::cout << "You have " << inventory[toUZ(type)] << ' ';
-		std::cout << (plural ? getItemNamePlural(type) : getItemNameSingular(type)) << '\n';
-}
-
-int totalCount(const std::vector<int>& inventory)
-{
-	int sum{ 0 };
-	for (auto e : inventory)
-		sum += e;
-	return sum;
+	std::size_t min{ 0 };
+	std::size_t max{ 0 };
+	for (std::size_t i{ 0 }; i < arr.size(); ++i)
+	{
+		if (arr[i] < arr[min]) min = i;
+		if (arr[i] > arr[max]) max = i;
+	}
+	return { min, max };
 }
 
 int main()
 {
-	std::vector inventory{ 1, 5, 10 };
-	assert(std::size(inventory) == Item::maxCount);
+	std::vector v1 { 3, 8, 2, 5, 7, 8, 3 };
+	std::vector v2 { 5.5, 2.7, 3.3, 7.6, 1.2, 8.8, 6.6 };
 
-	for (int i{ 0 }; i < Item::maxCount; ++i)
-	{
-		auto item{ static_cast<Item::ItemTypes>(i) };
-		print(inventory, item);
-	}
-	std::cout << "You have " << totalCount(inventory) << " total items\n";
+	auto m1{ minMax(v1) };
+	std::cout << "The min element has index " << m1.first << " and the value " << v1[m1.first] << '\n';
+	std::cout << "The max element has index " << m1.second << " and the value " << v1[m1.second] << "\n\n";
+
+	auto m2{ minMax(v2) };
+	std::cout << "The min element has index " << m2.first << " and value " << v2[m2.first] << '\n';
+	std::cout << "The max element has index " << m2.second << " and value " << v2[m2.second] << '\n';
 	return 0;
 }
