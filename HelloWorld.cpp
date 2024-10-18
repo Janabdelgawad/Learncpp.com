055bc200959eba0c3c2a036bae9ee8822ae964a9
JANA <janaakamaal2004@gmail.com>
2024-10-18 22:25:20 +0300

    Question 4, main function update. Guessed en fe stack operation bs m3rftsh ataba2 brdo copied el haga yarab athasen :(

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,30 +1,80 @@
+/*
+user can enter as many integers as they like.
+Stop accepting input when the user enters -1.
+
+Print the vector and find the min and max elements.
+
+When run with the input 3 8 5 2 3 7 -1, the program should produce the following output:
+Enter numbers to add (use -1 to stop): 3 8 5 2 3 7 -1
+With array ( 3, 8, 5, 2, 3, 7 ):
+Do something reasonable when the user enters -1 as the first input.
+*/
 #include <iostream>
 #include <vector>
-#include <utility>
+
 template <typename T>
-std::pair<std::size_t, std::size_t> minMax(const std::vector<T>& arr)
+std::pair<std::size_t, std::size_t> findMinMaxIndices(const std::vector<T>& v)
 {
-	std::size_t min{ 0 };
-	std::size_t max{ 0 };
-	for (std::size_t i{ 0 }; i < arr.size(); ++i)
-	{
-		if (arr[i] < arr[min]) min = i;
-		if (arr[i] > arr[max]) max = i;
-	}
-	return { min, max };
+    std::size_t min{ 0 };
+    std::size_t max{ 0 };
+    for (std::size_t index{ 0 }; index < v.size(); ++index)
+    {
+        if (v[index] < v[min])
+            min = index;
+        if (v[index] > v[max])
+            max = index;
+    }
+
+    return { min, max };
+}
+
+template <typename T>
+void printArray(const std::vector<T>& v)
+{
+    bool comma{ false };
+    std::cout << "With array ( ";
+    for (const auto& e : v)
+    {
+        if (comma)
+            std::cout << ", ";
+
+        std::cout << e;
+        comma = true;
+    }
+    std::cout << " ):\n";
 }
 
 int main()
 {
-	std::vector v1 { 3, 8, 2, 5, 7, 8, 3 };
-	std::vector v2 { 5.5, 2.7, 3.3, 7.6, 1.2, 8.8, 6.6 };
+    std::vector<int> v1{};
+    std::cout << "Enter numbers to add (use -1 to stop): ";
 
-	auto m1{ minMax(v1) };
-	std::cout << "The min element has index " << m1.first << " and the value " << v1[m1.first] << '\n';
-	std::cout << "The max element has index " << m1.second << " and the value " << v1[m1.second] << "\n\n";
+    while (true)
+    {
+        int input{};
+        std::cin >> input;
+        if (input == -1) break;
 
-	auto m2{ minMax(v2) };
-	std::cout << "The min element has index " << m2.first << " and value " << v2[m2.first] << '\n';
-	std::cout << "The max element has index " << m2.second << " and value " << v2[m2.second] << '\n';
-	return 0;
+        if (!std::cin)
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+
+        v1.push_back(input);
+
+    }
+
+    if (v1.size() == 0) std::cout << "Array is empty.\n";
+
+    else
+    {
+        printArray(v1);
+        auto m1{ findMinMaxIndices(v1) };
+        std::cout << "The min element has index " << m1.first << " and value " << v1[m1.first] << '\n';
+        std::cout << "The max element has index " << m1.second << " and value " << v1[m1.second] << '\n';
+
+    }
+    return 0;
 }
