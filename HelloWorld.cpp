bc34a554e39a4bb7ad9100d530c66d536e04e414
JANA <janaakamaal2004@gmail.com>
2024-10-22 01:31:24 +0300

    cheated some but overall good and became a bit more stress tolerant :)

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,29 +1,83 @@
 #include <iostream>
 #include <array>
-struct Item
+#include <string>
+namespace Animal
 {
-	std::string_view name{};
-	int gold{ 0 };
-};
-
-template <std::size_t N>
-void printStore(const std::array<Item, N>& arr)
-{
-	for (const auto& i : arr)
+	enum Type
 	{
-		std::cout << "A " << i.name << " costs " << i.gold << " gold.\n";
-	}
+		chicken, dog, cat, elephant, duck, snake, max_animals,
+	};
+
+	struct Data
+	{
+		std::string_view name{};
+		int legs{};		
+		std::string_view sound{};
+	};
+
+	constexpr std::array types { chicken, dog, cat, elephant, duck, snake };
+
+	constexpr std::array data{
+		Data{ "chicken",    2, "cluck" },
+		Data{ "dog",        4, "woof" },
+		Data{ "cat",        4, "meow" },
+		Data{ "elephant",   4, "pawoo" },
+		Data{ "duck",       2, "quack" },
+		Data{ "snake",      0, "hissss" },
+	};
+
+	static_assert(std::size(types) == Type::max_animals);
+	static_assert(std::size(data)  == Type::max_animals);
 }
-//not explicitly specify the element type for each initializer.
+
+std::istream& operator>> (std::istream& in, Animal::Type& animal)
+{
+	std::string input{};
+	std::getline(in >> std::ws, input);
+
+	// find matching name
+	for (std::size_t index = 0; index < Animal::data.size(); ++index)
+	{
+		if (input == Animal::data[index].name)
+		{
+			// If we found a matching name, we can get the enumerator value based on its index
+			animal = static_cast<Animal::Type>(index);
+			return in;
+		}
+	}
+	// no match, so set input stream to fail state
+	in.setstate(std::ios_base::failbit);
+
+	return in;
+}
+
+void printAnimalData(Animal::Type type)
+{
+	const Animal::Data& animal{ Animal::data[type] };
+	std::cout << "A " << animal.name << " has " << animal.legs << " legs and says " << animal.sound << ".\n";
+
+}
+
 int main()
 {
-	constexpr std::array<Item, 4> arr 
-	{ {
-		{"sword", 5},
-		{"dagger", 3},
-		{"club", 2},
-		{"spear", 7}
-	} };
-	printStore(arr);
+	std::cout << "Enter an animal: ";
+	Animal::Type input{};
+	std::cin >> input;
+
+	if (!std::cin)
+	{
+		std::cin.clear();
+		std::cout << "That animal couldn't be found.\n";
+		input = Animal::max_animals;
+	}
+	else
+		printAnimalData(input);
+
+    std::cout << "\nHere is the data for the rest of the animals:\n";
+	for (auto a : Animal::types)
+	{
+		if (a != input)
+            printAnimalData(a);
+	}
 	return 0;
 }
