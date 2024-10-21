bce141caeac8b72c9169135a3b6355871f88abbd
JANA <janaakamaal2004@gmail.com>
2024-10-21 23:47:12 +0300

    Question 1

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,32 +1,28 @@
 #include <iostream>
 #include <array>
-/*
-When run, it should print :
-
-The array(1, 4, 9, 16) has length 4
-The array(h, e, l, l, o) has length 5
-*/
-template <typename T, std::size_t N>
-void printArray(const std::array<T, N>& arr)
+struct Item
 {
-    bool first{ true };
-    std::cout << "The array(";
-    for (const auto& i : arr)
-    {
-        std::cout << (first ? "" : ", ");
-        std::cout << i;
-        first = false;
-    }
-    std::cout << ") has length " << N << '\n';
-}
+	std::string_view name{};
+	int gold{ 0 };
+};
 
+template <std::size_t N>
+void printStore(const std::array<Item, N>& arr)
+{
+	for (const auto& i : arr)
+	{
+		std::cout << "A " << i.name << " costs " << i.gold << " gold.\n";
+	}
+}
 int main()
 {
-    constexpr std::array arr1{ 1, 4, 9, 16 };
-    printArray(arr1);
-
-    constexpr std::array arr2{ 'h', 'e', 'l', 'l', 'o' };
-    printArray(arr2);
-
-    return 0;
+	constexpr std::array arr 
+	{
+		Item {"sword", 5},
+		Item {"dagger", 3},
+		Item {"club", 2},
+		Item {"spear", 7}
+	};
+	printStore(arr);
+	return 0;
 }
