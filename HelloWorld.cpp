3eb51d4e53f7a3d4d38a487c0f8f71443b3b0632
JANA <janaakamaal2004@gmail.com>
2024-10-21 21:11:49 +0300

    qqq

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <array>
+/*
+When run, it should print :
+
+The array(1, 4, 9, 16) has length 4
+The array(h, e, l, l, o) has length 5
+*/
+template <typename T, std::size_t N>
+void printArray(const std::array<T, N>& arr)
+{
+    bool first{ true };
+    std::cout << "The array(";
+    for (const auto& i : arr)
+    {
+        std::cout << (first ? "" : ", ");
+        std::cout << i;
+        first = false;
+    }
+    std::cout << ") has length " << N << '\n';
+}
 
 int main()
 {
-	constexpr std::array<char, 5> hello{ 'h', 'e', 'l', 'l', 'o' };
-	std::cout << "The length is: " << std::size(hello) << '\n';
-	std::cout << hello[1] << hello.at(1) << std::get<1>(hello) << '\n';
-	return 0;
+    constexpr std::array arr1{ 1, 4, 9, 16 };
+    printArray(arr1);
+
+    constexpr std::array arr2{ 'h', 'e', 'l', 'l', 'o' };
+    printArray(arr2);
+
+    return 0;
 }
