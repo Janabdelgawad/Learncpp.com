c56b653b5baab92623bbefa3a76516044d94ac8a
JANA <janaakamaal2004@gmail.com>
2024-10-21 20:29:02 +0300

    aaaa

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-	std::array<double, 365> a{};
 	constexpr std::array<char, 5> hello{ 'h', 'e', 'l', 'l', 'o' };
-	std::cout << hello[1] << '\n';
+	std::cout << "The length is: " << std::size(hello) << '\n';
+	std::cout << hello[1] << hello.at(1) << std::get<1>(hello) << '\n';
 	return 0;
 }
