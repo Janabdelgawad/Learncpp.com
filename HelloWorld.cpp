50023b32e868020b34e0e6fe05cea83e6682b1c6
JANA <janaakamaal2004@gmail.com>
2024-10-22 22:10:15 +0300

    find the mistakes

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,18 @@
+//What three things are wrong with the following program ?
+
+#include <iostream>
+
 int main()
 {
-//Convert the following std::array definition 
-// to an equivalent constexpr C - style array definition:
+    int length{ 5 };
+    const int arr[length]{ 9, 7, 5, 3, 1 };
 
-	constexpr int a[3] {}; // allocate 3 ints
-	return 0;
+    std::cout << arr[length];
+    arr[0] = 4;
+
+    return 0;
 }
+//1.cant re-assign a const array -> arr[0] = 4; right
+//x.don't need to provide array length if all array elements are intialized not important
+//3.defining a const object with a non const one - must be compile time constant right
+//2.arrays are indexed from 0 to N-1 so length = 5 when array has 5 elements - is out of bounds
