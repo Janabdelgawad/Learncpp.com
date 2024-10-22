722b625325e320750ac3cc81b60690488fc14888
JANA <janaakamaal2004@gmail.com>
2024-10-22 22:36:07 +0300

    Question 3: didn't delete my code to fit into the guide answer's mold but altered some of my own to be able to run :)

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,18 +1,48 @@
-//What three things are wrong with the following program ?
+/*
+A “perfect square” is a natural number whose square root is an integer.
+We can make perfect squares by multiplying a natural number (including zero) by itself.
+The first 4 perfect squares are: .
 
+Repeatedly ask the user to enter a single digit integer, or -1 to quit.
+Print whether the digit the user entered is a perfect square.
+
+The output should match the following:
+
+Enter a single digit integer, or -1 to quit: 4
+4 is a perfect square
+
+Enter a single digit integer, or -1 to quit: 5
+5 is not a perfect square
+
+Enter a single digit integer, or -1 to quit: -1
+Bye
+Hints: Use a range-based for loop to traverse the C-style array to look for a match.
+*/
 #include <iostream>
 
 int main()
 {
-    int length{ 5 };
-    const int arr[length]{ 9, 7, 5, 3, 1 };
+	constexpr int perfectSquaresArray[] {0, 1, 4, 9};
 
-    std::cout << arr[length];
-    arr[0] = 4;
+	while(true)
+	{
+		std::cout << "Enter a single digit integer, or -1 to quit: ";
+		int input{};
+		std::cin >> input;
 
-    return 0;
+		if (input == -1) break;
+
+		bool in{ false };
+		for (const auto& i : perfectSquaresArray)
+			if (input == i) in = true;
+
+		if (in) std::cout << input << " is a perfect square.\n";
+
+		else std::cout << input << " is not a perfect square.\n";
+	}
+
+	std::cout << "Bye.\n";
+
+	return 0;
 }
-//1.cant re-assign a const array -> arr[0] = 4; right
-//x.don't need to provide array length if all array elements are intialized not important
-//3.defining a const object with a non const one - must be compile time constant right
-//2.arrays are indexed from 0 to N-1 so length = 5 when array has 5 elements - is out of bounds
+
