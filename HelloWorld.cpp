d893fdb931b754e76f4cf1104b0e0fdf1814e912
JANA <janaakamaal2004@gmail.com>
2024-10-23 05:23:07 +0300

    question 1: traverse a c style string and print it's content character by character

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,48 +1,24 @@
 /*
-A “perfect square” is a natural number whose square root is an integer.
-We can make perfect squares by multiplying a natural number (including zero) by itself.
-The first 4 perfect squares are: .
-
-Repeatedly ask the user to enter a single digit integer, or -1 to quit.
-Print whether the digit the user entered is a perfect square.
-
-The output should match the following:
-
-Enter a single digit integer, or -1 to quit: 4
-4 is a perfect square
-
-Enter a single digit integer, or -1 to quit: 5
-5 is not a perfect square
-
-Enter a single digit integer, or -1 to quit: -1
-Bye
-Hints: Use a range-based for loop to traverse the C-style array to look for a match.
+Use a pointer and pointer arithmetic
+to step through each character of the string
+and print that character.
 */
 #include <iostream>
+void print(const char* words)
+{
+	const char* begin{ words };
+	const char* end{words + std::strlen(words) };
+
+	for (;begin != end;++begin)
+	{
+		std::cout << *begin << ' ';
+	}
+}
 
 int main()
 {
-	constexpr int perfectSquaresArray[] {0, 1, 4, 9};
-
-	while(true)
-	{
-		std::cout << "Enter a single digit integer, or -1 to quit: ";
-		int input{};
-		std::cin >> input;
-
-		if (input == -1) break;
-
-		bool in{ false };
-		for (const auto& i : perfectSquaresArray)
-			if (input == i) in = true;
-
-		if (in) std::cout << input << " is a perfect square.\n";
-
-		else std::cout << input << " is not a perfect square.\n";
-	}
-
-	std::cout << "Bye.\n";
-
+	const char cString[] {"Hello, world!"};
+	print(cString);
 	return 0;
 }
 
