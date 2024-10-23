9a5c0ff7b5339821a3a8a73898eb4bf3f5c60818
JANA <janaakamaal2004@gmail.com>
2024-10-23 05:42:19 +0300

    Question2 : same as question 1 but reversed the string

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,22 +1,32 @@
+//Repeat quiz #1, but this time the function should print the string backwards.
+
+/*
+2 copies of pointers to strings 
+1st pointer(main) stays the same at index 0
+2nd pointer(ptr) moves until it reaches the end of the string "null terminator"
+now 2nd pointer reached the end make it wake backward until it reaches main pointer
+*/
+
 #include <iostream>
 //pointer to first char of string words
 void print(const char words[])
 {
-	//as long as pointer doesnt point to a null terminator
-	while (*words != '\0')
+	const char* ptr{ words };
+
+	while (*ptr != '\0') ++ptr;
+
+	while (ptr-- != words)
 	{
 		//dereference and print the value
-		std::cout << *words << ' ';
-		//increment the pointer to point to the next char in line
-		++words;
+		std::cout << *ptr << ' ';
 	}
-
 }
 
 int main()
 {
 	const char cString[] {"Hello, world!"};
 	print(cString);
+	std::cout << '\n';
 	return 0;
 }
 
