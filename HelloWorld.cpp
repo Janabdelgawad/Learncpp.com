2f938b8c955643a4d38f3aac11f9f2e5789968b1
JANA <janaakamaal2004@gmail.com>
2024-11-02 19:08:54 +0200

    Question 1: recursive factorial function, run 7 times.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,27 +1,13 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
+int factorial(int N)
+{
+	if (N <= 0) return 1;
+
+	return factorial(N - 1) * N;
+}
 int main()
 {
-	std::cout << "How many names would you like to enter: ";
-	std::size_t num{};
-	std::cin >> num;
-
-	auto* names{ new std::string[num]{} };
-
-	for (std::size_t i{0}; i < num; ++i)
-	{
-		std::cout << "Enter name #" << i + 1 << ": ";
-		std::getline(std::cin >> std::ws, names[i]);
-	}
-
-	std::sort(names, names + num);
-
-	std::cout << "\nHere is your sorted list: \n";
-
-	for (std::size_t i{ 0 }; i < num; ++i)
-		std::cout << "Name #" << i + 1 << ": " << names[i] << '\n';
-
-	delete[] names;
+	for (int i{ 0 }; i < 7; ++i)
+		std::cout << factorial(i) << '\n';
 	return 0;
 }
