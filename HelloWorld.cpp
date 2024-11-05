eab5344298db0a3b0bb3257bf324708767e9de74
JANA <janaakamaal2004@gmail.com>
2024-11-05 17:13:43 +0200

    Question 2: overloading the comparison operator < and the std::ostream operator

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,62 +1,63 @@
+#include <algorithm>
 #include <iostream>
-#include <numeric> // for std::gcd
+#include <string>
+#include <vector>
 
-class Fraction
+class Car
 {
 private:
-	int m_numerator{};
-	int m_denominator{};
+    std::string m_make;
+    std::string m_model;
 
 public:
-	Fraction(int numerator = 0, int denominator = 1) :
-		m_numerator{ numerator }, m_denominator{ denominator }
-	{
-		// We put reduce() in the constructor to ensure any new fractions we make get reduced!
-		// Any fractions that are overwritten will need to be re-reduced
-		reduce();
-	}
+    Car(std::string_view make, std::string_view model)
+        : m_make{ make }, m_model{ model }
+    {
+    }
 
-	void reduce()
-	{
-		int gcd{ std::gcd(m_numerator, m_denominator) };
-		if (gcd)
-		{
-			m_numerator /= gcd;
-			m_denominator /= gcd;
-		}
-	}
+    friend bool operator== (const Car& c1, const Car& c2);
+    friend bool operator!= (const Car& c1, const Car& c2);
 
-	friend std::ostream& operator<<(std::ostream& out, const Fraction& f1);
+    friend std::ostream& operator<< (std::ostream& out, const Car& c)
+    {
+        out << '(' << c.m_make << ", " << c.m_model << ")";
+        return out;
+    }
 
-	friend bool operator== (const Fraction& f1, const Fraction& f2) { return (f1.m_numerator == f2.m_numerator) && (f1.m_denominator == f2.m_denominator); }
-	//convert sign, not order of parameters
-	friend bool operator!= (const Fraction& f1, const Fraction& f2) { return !(operator==(f1, f2)); }
-	//perform criss/cross multiplication before comparison
-	friend bool operator<  (const Fraction& f1, const Fraction& f2) { return (f1.m_numerator * f2.m_denominator) < (f2.m_numerator * f1.m_denominator); }
-	//convert sign and order or parameters
-	friend bool operator>  (const Fraction& f1, const Fraction& f2) { return   operator<(f2, f1); }
-	//convert signs, not order of parameters
-	friend bool operator<= (const Fraction& f1, const Fraction& f2) { return !(operator>(f1, f2)); }
-	friend bool operator>= (const Fraction& f1, const Fraction& f2) { return !(operator<(f1, f2)); }
-
+    friend bool operator<(const Car& c1, const Car& c2)
+    {
+        //if the car isn't the same
+        if (c1.m_make != c2.m_make)
+            return c1.m_make < c2.m_make; //compare the MAKE normally
+        return c1.m_model < c2.m_model; // else if they are the same, compare the MODEL
+    }
 };
 
-std::ostream& operator<<(std::ostream& out, const Fraction& f1)
+bool operator== (const Car& c1, const Car& c2)
 {
-	out << f1.m_numerator << '/' << f1.m_denominator;
-	return out;
+    return (c1.m_make == c2.m_make &&
+        c1.m_model == c2.m_model);
+}
+
+bool operator!= (const Car& c1, const Car& c2)
+{
+    return (c1.m_make != c2.m_make ||
+        c1.m_model != c2.m_model);
 }
 
 int main()
 {
-	Fraction f1{ 3, 2 };
-	Fraction f2{ 5, 8 };
+    std::vector<Car> cars{
+      { "Toyota", "Corolla" },
+      { "Honda", "Accord" },
+      { "Toyota", "Camry" },
+      { "Honda", "Civic" }
+    };
 
-	std::cout << f1 << ((f1 == f2) ? " == " : " not == ") << f2 << '\n';
-	std::cout << f1 << ((f1 != f2) ? " != " : " not != ") << f2 << '\n';
-	std::cout << f1 << ((f1 < f2) ? " < " : " not < ") << f2 << '\n';
-	std::cout << f1 << ((f1 > f2) ? " > " : " not > ") << f2 << '\n';
-	std::cout << f1 << ((f1 <= f2) ? " <= " : " not <= ") << f2 << '\n';
-	std::cout << f1 << ((f1 >= f2) ? " >= " : " not >= ") << f2 << '\n';
-	return 0;
+    std::sort(cars.begin(), cars.end()); // requires an overloaded operator<
+
+    for (const auto& car : cars)
+        std::cout << car << '\n'; // requires an overloaded operator<<
+
+    return 0;
 }
