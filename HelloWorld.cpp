daeb1c263d3dcfa0d882a9f63c5021e6bdba0ffa
JANA <janaakamaal2004@gmail.com>
2024-11-16 22:42:09 +0200

    commit sickofthisshit

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,99 +1,35 @@
 #include <iostream>
-#include <cassert>
-class IntArray
+#include <memory>
+class Fraction
 {
 private:
-	int m_size{0};
-	int* L{ nullptr };
+	int m_numerator{ 0 };
+	int m_denominator{ 1 };
 
 public:
-	explicit IntArray(int size) : m_size{size}
+	Fraction(int numerator = 0, int denominator = 1) :
+		m_numerator{ numerator }, m_denominator{ denominator }
 	{
-		assert(size > 0 && "Invalid input");
-		L = new int[static_cast<std::size_t>(m_size)]{};
-	};
-
-	//copy constructor that does deep copying
-	IntArray(const IntArray& array) : m_size{ array.m_size }
-	{
-		L = new int[static_cast<std::size_t>(m_size)] {};
-		for (int count{ 0 }; count < array.m_size; ++count)
-			L[count] = array.L[count];
 	}
 
-	~IntArray()
+	friend std::ostream& operator<<(std::ostream& out, const Fraction& f1)
 	{
-		delete[] L;
-	}
-
-	friend std::ostream& operator<<(std::ostream& out, const IntArray& size);
-
-	int& operator[] (const int index)
-	{
-		assert(index >= 0);
-		assert(index < m_size);
-		return L[index];
-	}
-
-	IntArray& operator= (const IntArray& array)
-	{
-		// self-assignment guard
-		if (this == &array)
-			return *this;
-
-		// If this array already exists, delete it so we don't leak memory
-		delete[] L;
-
-		m_size = array.m_size;
-
-		// Allocate a new array
-		L = new int[static_cast<std::size_t>(m_size)] {};
-
-		// Copy elements from original array to new array
-		for (int count{ 0 }; count < array.m_size; ++count)
-			L[count] = array.L[count];
-
-		return *this;
+		out << f1.m_numerator << '/' << f1.m_denominator;
+		return out;
 	}
 };
 
-std::ostream& operator<<(std::ostream& out, const IntArray& f)
+void printFraction(const Fraction* ptr)
 {
-	for (int count{ 0 }; count < f.m_size; ++count)
-	{
-		out << f.L[count] << ' ';
-	}
-	return out;
-}
-
-IntArray fillArray()
-{
-	IntArray a(5);
-
-	a[0] = 5;
-	a[1] = 8;
-	a[2] = 2;
-	a[3] = 3;
-	a[4] = 6;
-
-	return a;
+	if (ptr)
+		std::cout << *ptr << '\n';
+	else
+		std::cout << "No fraction\n";
 }
 
 int main()
 {
-	IntArray a{ fillArray() };
-
-	std::cout << a << '\n';
-
-	auto& ref{ a }; // we're using this reference to avoid compiler self-assignment errors
-	a = ref;
-
-	IntArray b(1);
-	b = a;
-
-	a[4] = 7;
-
-	std::cout << b << '\n';
-
+	auto ptr{ std::make_unique<Fraction>(3, 5) };
+	printFraction(ptr.get());
 	return 0;
 }
