727c6f00df3238804797b18aae776e8c78ebea8c
JANA <janaakamaal2004@gmail.com>
2024-12-09 22:07:56 +0200

    A templated class allows user to define one template type
    that is used for both values in a pair.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,95 +1,26 @@
-#include <vector>
 #include <iostream>
-
-class Point
-{
+template <typename T>
+class Pair1 {
 private:
-	int m_x{};
-	int m_y{};
-
+	T m_x{};
+	T m_y{};
 public:
-	Point(int x, int y)
-		: m_x{ x }, m_y{ y }
-	{
+	Pair1(const T& x, const T& y)
+		: m_x{ x }, m_y{ y } {}
 
-	}
-
-	friend std::ostream& operator<<(std::ostream& out, const Point& p)
-	{
-		return out << "Point(" << p.m_x << ", " << p.m_y << ')';
-	}
+	T& first() { return m_x; }
+	T& second() { return m_y; }
+	const T& first() const { return m_x; }
+	const T& second() const { return m_y; }
 };
-
-class Shape
-{
-public:
-	virtual std::ostream& print(std::ostream& out) const = 0;
-	friend std::ostream& operator<<(std::ostream& out, const Shape& p)
-	{
-		return p.print(out);
-	}
-	virtual ~Shape() = default;
-};
-
-class Triangle : public Shape
-{
-private:
-	Point m_p1;
-	Point m_p2;
-	Point m_p3;
-
-public:
-	Triangle(const Point& p1, const Point& p2, const Point& p3)
-		: m_p1{ p1 }, m_p2{ p2 }, m_p3{ p3 } {}
-
-	std::ostream& print(std::ostream& out) const override
-	{
-		return out << "Triangle(" << m_p1 << ", " << m_p2 << ", " << m_p3 << ')';
-	}
-};
-
-class Circle : public Shape
-{
-private:
-	Point m_center;
-	int m_radius;
-public:
-	Circle(const Point& center, const int radius) 
-		: m_center{ center }, m_radius{ radius } {}
-
-	std::ostream& print(std::ostream& out) const override
-	{
-		return out << "Circle(" << m_center << ", radius " << m_radius << ')';
-	}
-	int getRadius() const { return m_radius; }
-};
-
-int getLargestRadius(const std::vector<Shape*>& v)
-{
-	int max{ 0 };
-	for (const auto* i : v)
-	{
-		if (auto * c{ dynamic_cast<const Circle*>(i) })
-			max = std::max(max, c->getRadius());
-	}
-	return max;
-}
-
 
 int main()
 {
-	std::vector<Shape*> v{
-	  new Circle{Point{ 1, 2 }, 7},
-	  new Triangle{Point{ 1, 2 }, Point{ 3, 4 }, Point{ 5, 6 }},
-	  new Circle{Point{ 7, 8 }, 3}
-	};
+	Pair1<int> p1{ 5, 8 };
+	std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
 
-	// print each shape in vector v on its own line here
-	for (const auto* i : v) std::cout << *i << '\n';
+	const Pair1<double> p2{ 2.3, 4.5 };
+	std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
 
-	std::cout << "The largest radius is: " << getLargestRadius(v) << '\n'; // write this function
-
-	// delete each element in the vector here
-	for (const auto* i : v) delete i;
 	return 0;
 }
